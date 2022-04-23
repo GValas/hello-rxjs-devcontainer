6dbc9baccd4db0e9534253639aba198e82e022ab
gege <valasg@gmail.com>
2022-04-23 17:17:45 +0000

    stringify output

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,21 @@ import { cfg } from "./config";
 import { throttle } from "rxjs/operators";
 
 // arrays of observables
-const nbOfStocks = 10
+const nbOfStocks = 10;
 const noises = noisesFlush(nbOfStocks);
 const stocks = stocksFlush(noises);
 const options = optionsFlush(stocks, [100, 110], [1, 2]);
 
 // subscring
-combineLatest(options)  // stocks
+combineLatest(options) // stocks
   .pipe(throttle(() => interval(cfg.throttle)))
   .subscribe((assets) =>
-  assets.map((z, i) => console.log(i + 1, JSON.stringify(z)))
+    assets.map((z, i) =>
+      console.log(
+        i + 1,
+        JSON.stringify(z, (key, val) =>
+          val.toFixed ? Number(val.toFixed(2)) : val
+        )
+      )
+    )
   );
